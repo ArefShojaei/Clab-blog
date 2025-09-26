d21fe5ab66eb7fff5dd678746e3ee8bbe7fab07f
ArefShojaei <arefshojaei82dev@gmail.com>
2025-09-26 22:15:26 +0330

    ADD: add auth route names

--- modules/auth/main.php
+++ modules/auth/main.php
@@ -8,8 +8,8 @@ import("@modules/auth/_controller");
  * Auth routes
  */
 groupRoute("/auth", function() {
-    addRoute("GET", "/login", "__auth__login_index");
+    addRoute("GET", "/login", "__auth__login_index", name:"auth.login");
     addRoute("POST", "/login", "__auth__login_store");
-    addRoute("GET", "/register", "__auth__register_index");
+    addRoute("GET", "/register", "__auth__register_index", name:"auth.register");
     addRoute("POST", "/register", "__auth__register_store");
 });
